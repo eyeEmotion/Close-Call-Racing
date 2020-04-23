3f2f23bdc35e3b848c300e8859e0e270078b3c3f
eyeEmotion <62855049+eyeEmotion@users.noreply.github.com>
2020-04-23 22:49:08 +0200

    Update Camera.gd

--- Camera.gd
+++ Camera.gd
@@ -1,11 +1,18 @@
 extends Camera
 
 export (NodePath) var follow_this_path = null
-export var target_distance = 3.0
-export var target_height = 2.0
+export var target_distance = 4.0
+export var target_height = 1.0
 
 var follow_this = null
 var last_lookat
+var look_around = 0
+
+var minFOV = 70.0
+var maxFOV = 85.0
+var boostFOV = 110.0
+var currentFOV = minFOV
+var spdChangeFOV = 0.01
 
 func _ready():
 	follow_this = get_node(follow_this_path)
@@ -14,7 +21,7 @@ func _ready():
 func _physics_process(delta):
 	var delta_v = global_transform.origin - follow_this.global_transform.origin
 	var target_pos = global_transform.origin
-	
+
 	# ignore y
 	delta_v.y = 0.0
 	
@@ -24,16 +31,34 @@ func _physics_process(delta):
 		target_pos = follow_this.global_transform.origin + delta_v
 	else:
 		target_pos.y = follow_this.global_transform.origin.y + target_height
-	
-	# This part is the stuff I added to make the camera look in the direction of where you are steering at
-	# the equation works, but h_offset is the wrong property to adress
-	# need to be able to pass this of to the x-value of where the camera is looking at
-	#	
-	#if (follow_this.fwd_mps >= 0):
-	#	h_offset = (follow_this.fwd_mps / 8) * -(follow_this.steering / 3)
-	
+
 	global_transform.origin = global_transform.origin.linear_interpolate(target_pos, delta * 80.0)
 	
 	last_lookat = last_lookat.linear_interpolate(follow_this.global_transform.origin, delta * 60.0)
 	
 	look_at(last_lookat, Vector3(0.0, 0.6, 0.0))
+
+
+func _process(delta: float) -> void:
+	#this part is obviously wrong
+	
+	look_around = Input.get_action_strength("look_left") - Input.get_action_strength("look_right")
+	h_offset = look_around * 3
+	
+	#--end wrong--
+
+	if (follow_this.boost):
+		currentFOV = boostFOV
+	else:
+		currentFOV = maxFOV
+
+	if (follow_this.fwd_mps > 150):
+		if (get_fov() < currentFOV):
+			set_fov(get_fov() + delta * 7)
+			target_distance = lerp(target_distance, 3.0, spdChangeFOV)
+			target_height = lerp(target_height, 0.5, spdChangeFOV)
+	else:
+		if (get_fov() > minFOV):
+			set_fov(get_fov() - delta * 7)			
+			target_distance = lerp(target_distance, 4.0, spdChangeFOV)
+			target_height = lerp(target_height, 1.0, spdChangeFOV)
